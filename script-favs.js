d5dbf357bb223fa0222d3a79291b24cf1fbfd4f2
Samuel Gesang <sgesang@mailo.com>
2024-01-15 15:43:37 +0100

    add JS

---
+++ script-favs.js
@@ -0,0 +1,60 @@
+async function loadQuestions() {
+  try {
+    const response = await fetch("questions.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fehler beim Laden der JSON-Datei:", error);
+    return [];
+  }
+}
+
+async function init() {
+  const questions = await loadQuestions();
+  const mainDiv = document.querySelector("main");
+  displayQuestions(questions, mainDiv);
+
+  const showAnswerButtons = document.querySelectorAll("button");
+  showAnswerButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const answer = this.nextElementSibling;
+      answer.style.display == "none"
+        ? (answer.style.display = "block")
+        : (answer.style.display = "none");
+    });
+  });
+
+  const bookmarkIcons = document.querySelectorAll(".bookmark");
+  bookmarkIcons.forEach(function (icon) {
+    icon.addEventListener("click", function () {
+      const article = this.parentElement;
+      article.classList.contains("article-fav")
+        ? article.classList.remove("article-fav")
+        : article.classList.add("article-fav");
+    });
+  });
+}
+
+init();
+
+function displayQuestions(questions, target) {
+  favQuestions = questions.filter((q) => q.bookmarked);
+  for (question of favQuestions) {
+    target.innerHTML += `
+    <article class="article ${question.bookmarked ? "article-fav" : ""}">
+        <div class="bookmark"><i class="fas fa-bookmark"></i></div>
+        <div class="headline">${question.headline}</div>
+        <button>show answer</button>
+        <div class="answer">${question.answer}</div>
+        <ul class="tags">
+          <li class="tag">html</li>
+          <li class="tag">flexbox</li>
+          <li class="tag">css</li>
+        </ul>
+      </article>
+`;
+  }
+}
